c2c11db58e49d8f708a6e43696dbdba0302a7b14
Dang Pham <danggphamm@gmail.com>
2023-03-31 11:15:46 -0500

    First commit

---
+++ Assets/Scripts/Player/DragToMove.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragToMove : MonoBehaviour
+{
+    // Check if mouse clicked
+    bool clicked = false;
+
+    // Check if game is over/lost
+    bool gameOver = false;
+
+    public bool collidingWall = false;
+
+    public GameObject characterGraphic;
+
+    Vector3 lastPos = Vector3.zero;
+
+    Vector3 lastDir = Vector3.zero;
+
+    public GameObject wallDetector;
+
+    // Speed
+    public float speed;
+
+    // The coordinate where the mouse clicked
+    Vector3 mouseInitialPos = Vector3.zero;
+    Vector3 dir = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (!clicked)
+                {
+                    clicked = true;
+                    mouseInitialPos = Input.mousePosition;
+                }
+
+                // Direction the mouse dragging to
+                dir = mouseInitialPos - Input.mousePosition;
+
+                // Direction in 2D
+                Vector3 twoDdirection = new Vector3(dir.x, 0, dir.y);
+
+                twoDdirection.Normalize();
+
+                // Rotate the character to the goinVector3.zerog direction
+                //if (Vector3.zero - twoDdirection != Vector3.zero)
+                //{
+                //    Quaternion rotation = Quaternion.LookRotation(Vector3.zero - twoDdirection, Vector3.up);
+                //    characterGraphic.transform.rotation = rotation;
+                //}
+
+                if (!wallDetector.GetComponent<WallDetector>().collidingWall)
+                {
+                    lastPos = transform.position;
+                    characterGraphic.transform.position -= twoDdirection * speed*Time.deltaTime;
+                    //Debug.Log("Pressed");
+                }
+                else
+                {
+                    characterGraphic.transform.position = lastPos;
+                    characterGraphic.transform.position -= twoDdirection * speed * Time.deltaTime;
+                }
+            }
+            else
+            {
+                // Refresh the variables after releasing the mouse
+                clicked = false;
+                mouseInitialPos = Vector3.zero;
+                dir = Vector3.zero;
+                //GetComponent<Rigidbody>().velocity = Vector3.zero;
+                //Debug.Log("Not pressed");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/WallDetector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDetector : MonoBehaviour
+{
+    public Collision collidingObject = null;
+    public bool collidingWall = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collidingObject = collision;
+
+        if (collidingObject != null)
+        {
+            if (collidingObject.gameObject.tag.ToLower() == "wall")
+            {
+                collidingWall = true;
+            }
+            else
+            {
+                collidingWall = false;
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        collidingWall = false;
+        collidingObject = null;
+    }
+}
